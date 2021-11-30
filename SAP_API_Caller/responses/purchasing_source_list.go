e1abe38adae728dbfaf152f199d58be9b047d052
Yurino Oga <yurino.o@latona.io>
2021-11-30 19:31:37 +0900

    release

---
+++ SAP_API_Caller/responses/purchasing_source_list.go
@@ -0,0 +1,30 @@
+package responses
+
+type PurchasingSourceList struct {
+	D struct {
+		Results []struct {
+			Metadata struct {
+				ID   string `json:"id"`
+				URI  string `json:"uri"`
+				Type string `json:"type"`
+			} `json:"__metadata"`
+			Material                   string `json:"Material"`
+			Plant                      string `json:"Plant"`
+			SourceListRecord           string `json:"SourceListRecord"`
+			ValidityStartDate          string `json:"ValidityStartDate"`
+			ValidityEndDate            string `json:"ValidityEndDate"`
+			Supplier                   string `json:"Supplier"`
+			PurchasingOrganization     string `json:"PurchasingOrganization"`
+			SupplyingPlant             string `json:"SupplyingPlant"`
+			OrderQuantityUnit          string `json:"OrderQuantityUnit"`
+			PurchaseOutlineAgreement   string `json:"PurchaseOutlineAgreement"`
+			SupplierIsFixed            bool   `json:"SupplierIsFixed"`
+			SourceOfSupplyIsBlocked    bool   `json:"SourceOfSupplyIsBlocked"`
+			MRPSourcingControl         string `json:"MRPSourcingControl"`
+			LastChangeDateTime         string `json:"LastChangeDateTime"`
+			IssgPlantIsFixed           bool   `json:"IssgPlantIsFixed"`
+			PurOutlineAgreementIsFixed bool   `json:"PurOutlineAgreementIsFixed"`
+			SourceOfSupplyIsFixed      bool   `json:"SourceOfSupplyIsFixed"`
+		} `json:"results"`
+	} `json:"d"`
+}
--- SAP_API_Output_Formatter/type.go
+++ SAP_API_Output_Formatter/type.go
@@ -1,33 +1,33 @@
 package sap_api_output_formatter
 
 type PurchasingSourceListReads struct {
-	ConnectionKey        string `json:"connection_key"`
-	Result               bool   `json:"result"`
-	RedisKey             string `json:"redis_key"`
-	Filepath             string `json:"filepath"`
-	APISchema            string `json:"api_schema"`
-	MaterialCode         string `json:"material_code"`
-	Plant                string `json:"plant"`
-	SourceListRecord     string `json:"source_list_record"`
-	Deleted              bool   `json:"deleted"`
+	ConnectionKey    string `json:"connection_key"`
+	Result           bool   `json:"result"`
+	RedisKey         string `json:"redis_key"`
+	Filepath         string `json:"filepath"`
+	APISchema        string `json:"api_schema"`
+	MaterialCode     string `json:"material_code"`
+	Plant            string `json:"plant"`
+	SourceListRecord string `json:"source_list_record"`
+	Deleted          bool   `json:"deleted"`
 }
 
-PurchasingSourceList struct {
-	Material                   string   `json:"Material"`
-	Plant                      string   `json:"Plant"`
-	SourceListRecord           string   `json:"SourceListRecord"`
-	ValidityStartDate          string   `json:"ValidityStartDate"`
-	ValidityEndDate            string   `json:"ValidityEndDate"`
-	Supplier                   string   `json:"Supplier"`
-	PurchasingOrganization     string   `json:"PurchasingOrganization"`
-	SupplyingPlant             string   `json:"SupplyingPlant"`
-	OrderQuantityUnit          string   `json:"OrderQuantityUnit"`
-	PurchaseOutlineAgreement   string   `json:"PurchaseOutlineAgreement"`
-	SupplierIsFixed            bool     `json:"SupplierIsFixed"`
-	SourceOfSupplyIsBlocked    bool     `json:"SourceOfSupplyIsBlocked"`
-	MRPSourcingControl         string   `json:"MRPSourcingControl"`
-	LastChangeDateTime         string   `json:"LastChangeDateTime"`
-	IssgPlantIsFixed           bool     `json:"IssgPlantIsFixed"`
-	PurOutlineAgreementIsFixed bool     `json:"PurOutlineAgreementIsFixed"`
-	SourceOfSupplyIsFixed      bool     `json:"SourceOfSupplyIsFixed"`
+type PurchasingSourceList struct {
+	Material                   string `json:"Material"`
+	Plant                      string `json:"Plant"`
+	SourceListRecord           string `json:"SourceListRecord"`
+	ValidityStartDate          string `json:"ValidityStartDate"`
+	ValidityEndDate            string `json:"ValidityEndDate"`
+	Supplier                   string `json:"Supplier"`
+	PurchasingOrganization     string `json:"PurchasingOrganization"`
+	SupplyingPlant             string `json:"SupplyingPlant"`
+	OrderQuantityUnit          string `json:"OrderQuantityUnit"`
+	PurchaseOutlineAgreement   string `json:"PurchaseOutlineAgreement"`
+	SupplierIsFixed            bool   `json:"SupplierIsFixed"`
+	SourceOfSupplyIsBlocked    bool   `json:"SourceOfSupplyIsBlocked"`
+	MRPSourcingControl         string `json:"MRPSourcingControl"`
+	LastChangeDateTime         string `json:"LastChangeDateTime"`
+	IssgPlantIsFixed           bool   `json:"IssgPlantIsFixed"`
+	PurOutlineAgreementIsFixed bool   `json:"PurOutlineAgreementIsFixed"`
+	SourceOfSupplyIsFixed      bool   `json:"SourceOfSupplyIsFixed"`
 }
